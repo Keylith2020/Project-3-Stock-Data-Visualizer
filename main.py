87668d067f71bea317c7e1f833d7022d0e9ef682
mstittmds <166522003+mstittmds@users.noreply.github.com>
2024-10-17 17:03:49 -0500

    Update main.py
    Added a shell for the user to run through

--- main.py
+++ main.py
@@ -11,3 +11,21 @@ def user_input():
 def call_api():
 
     return
+
+yesCheck = "y"
+while yesCheck == "y":
+	stockSymbol = input("Enter the stock symbol for the company you want data for: ")
+
+	print("Enter the chart type you would like (1, 2): ")
+	graphNum = input("(1)Bar | (2) Line: ")
+
+	print("Select the Time Series of the chart you want to Generate: ")
+	print("(1)Intraday | (2)Daily | (3)Weekly | (4)Monthly")
+	timeType = input("Enter time series option (1, 2, 3, 4): ")
+
+	date1 = input("Enter the start Date (YYYY-MM-DD): ")
+
+	date2 = input("Enter the end Date (YYYY-MM-DD): ")
+
+	yesCheck = input("Would you like to view more stock data? Press 'y' to countinue: ")
+
